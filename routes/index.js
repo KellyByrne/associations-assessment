f386851592c2c994f3010328346c89871e2aa1f0
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-11-02 21:28:11 -0700

    basically done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('monk')('localhost/associations-assessment')
+var trails = db.get('trails')
+var parks = db.get('parks')
+var states = db.get('states')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	trails.find({}, function(err, data){
+		res.render('index', {trails: data});
+	})
 });
 
 module.exports = router;
